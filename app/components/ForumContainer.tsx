05fb82ac0bc7f075eb6433cd26cf1952fb4f0f10
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-25 19:14:33 +0100

    update document

--- app/components/ForumContainer.tsx
+++ app/components/ForumContainer.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-
+async function getForumPosts() {
+  const usersApiUrl = "https://forum-app-z6fe.onrender.com/forumPost";
+  const res = await fetch(usersApiUrl, {
+    cache: "no-store",
+  });
+  return res.json();
+}
 
 const ForumContainer = async () => {
-  
-  
+  const forumPosts = await getForumPosts();
+  console.log(forumPosts);
   return (
     <main>
-      <div className="forum-container"></div>
+      <div className="forum-container">
+        {forumPosts.map((forumPost: ForumPost) => (
+          <p key={forumPost.id}>{forumPost.title}</p>
+        ))}
+      </div>
     </main>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import { NextPage } from "next";
 import React from "react";
-
-async function getForumPosts() {
-  const usersApiUrl = "https://forum-app-z6fe.onrender.com/forumPost";
-  const res = await fetch(usersApiUrl, {
-    cache: "no-store",
-  });
-  return res.json();
-}
+import ForumContainer from "./components/ForumContainer";
 
 const Home: NextPage = async () => {
-  const forumPosts = await getForumPosts();
-  console.log(forumPosts);
   return (
     <main className="home">
-      {forumPosts.map((forumPost: ForumPost) => (
-        <p key={forumPost.id}>{forumPost.title}</p>
-      ))}
+      <div className="home-container">
+        <ForumContainer />
+      </div>
     </main>
   );
 };
